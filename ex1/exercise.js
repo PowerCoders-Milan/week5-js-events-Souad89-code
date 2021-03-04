d8b07d0b774158bd9fee047b2aee9f004dbae8e6
Souad Allahham <souadallahham89@gmail.com>
2021-03-04 12:09:09 +0100

    I finished ex1

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,16 @@
-// define your functions here
+/*# Bold on hover
 
-// call the functions and connect them to an event
+Write a JavaScript program to change the color of the bold words when the user hovers the link.*/
+
+let a = document.querySelector('a');
+let p = document.querySelector('p');
+let strongs = document.querySelectorAll('strong');
+a.addEventListener('mouseover', function () {
+    strongs.forEach(function (strong) {
+        strong.style.color = "red";  
+    });
+})
+
+/*## EXTRA (to do when everything is finished)
+
+Make the color random for each bold word when the user hovers.*/
